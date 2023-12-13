af155b33ff5a5ffc6cd56807edc4f775f0f74089
Marcos Hirazawa <hirazawa.marcos@gmail.com>
2023-12-13 16:40:21 -0300

    minor home fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Index() {
             <Head>
                 <title>intibiome</title>
             </Head>
-            <Home title="When it comes to caring for our most intimate areas, we've lost our connection. Embarrassed, unwilling, or unable to communicate with others, we’re needlessly neglecting the parts of our bodies that need it most. We're here to help. Providing you with the expertise, knowledge and products you need to feel confident in your personal care." />
+            <Home />
         </>
     );
 }
